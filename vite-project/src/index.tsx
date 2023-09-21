dc26f7de25e3de34574a2ce1060f58f632b2f8d8
Gitari65 <agitari65@gmail.com>
2023-09-21 15:36:21 +0300

    adding a slider

--- vite-project/src/index.tsx
+++ vite-project/src/index.tsx
@@ -1,7 +1,8 @@
 import Header from './components/Header'
 import Home from './components/Home'
+import Slider from './components/Slider'
 
 //export the components so that they can be used in other files
-export { Header, Home }
+export { Header, Home ,Slider}
 
 
