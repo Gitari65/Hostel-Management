eb99144d683f0660b14d5495b2540cfcbe42069e
Gitari65 <agitari65@gmail.com>
2023-09-24 21:35:42 +0300

    adding a slider

--- vite-project/src/components/Slider.tsx
+++ vite-project/src/components/Slider.tsx
@@ -3,52 +3,81 @@ import '../assets/css/style.css'
 
 const Slider = () => {
   return (
-    <div>
-     
-        {/* <!-- Hero Section Begin --> */}
-        
-
-    <div className="hero-slider">
-        <div className="slider-item">
-            <div className="single-slider-item set-bg" data-setbg="img/slider-1.jpg">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <h1>We hope you’ll enjoy <br />your stay.</h1>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-lg-12">
-                            <div className="slider-nav">
-                                <a href="#" className="single-slider-nav">
-                                    <img src="img/pooltable.jpg" alt=""></img>
-                                    <div className="nav-text active">
-                                        <p>Pool Table<i className="lnr lnr-arrow-right"></i></p>
-                                    </div>
-                                </a>
-                                <a href="#" className="single-slider-nav">
-                                    <img src="img/screenTv.jpg" alt=""></img>
-                                    <div className="nav-text">
-                                        <p>Entertainment Room<i className="lnr lnr-arrow-right"></i></p>
-                                    </div>
-                                </a>
-                                <a href="#" className="single-slider-nav last">
-                                    <img src="img/nav-3.jpg" alt=""></img>
-                                    <div className="nav-text">
-                                        <p>Restaurant<i className="lnr lnr-arrow-right"></i></p>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
  
-
+        <>
+        <head>
+        
+        </head>
+         {/* <!-- Hero Section Begin --> */}
+         <div id="carouselExampleDark" className="carousel carousel-dark slide">
+  <div className="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div className="carousel-inner">
+    <div className="carousel-item active" data-bs-interval="10000">
+      <img src="/img/slider-1.jpg" className="d-block w-100" alt="..."></img>
+      <div className="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Some representative placeholder content for the first slide.</p>
+      </div>
     </div>
+    <div className="carousel-item" data-bs-interval="2000">
+      <img src="/img/pooltable.jpg" className="d-block w-100" alt="..."></img>
+      <div className="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Some representative placeholder content for the second slide.</p>
+      </div>
+    </div>
+    <div className="carousel-item">
+      <img src="/img/screenTv.jpg" className="d-block w-100" alt="..."></img>
+      <div className="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Some representative placeholder content for the third slide.</p>
+      </div>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="false"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+
+<div id="carouselExampleIndicators" className="carousel slide">
+  <div className="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img src="..." className="d-block w-100" alt="..."></img>
+    </div>
+    <div className="carousel-item">
+      <img src="..." className="d-block w-100" alt="..."></img>
+    </div>
+    <div className="carousel-item">
+      <img src="..." className="d-block w-100" alt="..."></img>
+    </div>
+  </div>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
+  </button>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
+  </button>
+</div>
+
+
+</>
   )
 }
 
-export default Slider
+export default Slider;
