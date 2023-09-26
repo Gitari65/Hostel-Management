8b028e825e0a6c2531fb198f3c5cabfdfc58ae97
Gitari65 <agitari65@gmail.com>
2023-09-26 05:51:32 +0300

    editing the sliderlanding page

--- vite-project/src/components/Slider.tsx
+++ vite-project/src/components/Slider.tsx
@@ -9,7 +9,7 @@ const Slider = () => {
         
         </head>
          {/* <!-- Hero Section Begin --> */}
-         <div id="carouselExampleDark" className="carousel carousel-dark slide">
+         <div id="carouselExampleDark" className="carousel carousel-dark slide" data-bs-ride="carousel">
   <div className="carousel-indicators">
     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -17,21 +17,21 @@ const Slider = () => {
   </div>
   <div className="carousel-inner">
     <div className="carousel-item active" data-bs-interval="10000">
-      <img src="/img/slider-1.jpg" className="d-block w-100" alt="..."></img>
+      <img src="/img/building-pic3.jpg" className="d-block w-100" alt="..."></img>
       <div className="carousel-caption d-none d-md-block">
         <h5>First slide label</h5>
         <p>Some representative placeholder content for the first slide.</p>
       </div>
     </div>
     <div className="carousel-item" data-bs-interval="2000">
-      <img src="/img/pooltable.jpg" className="d-block w-100" alt="..."></img>
+      <img src="/img/building-pic2.jpg" className="d-block w-100" alt="..."></img>
       <div className="carousel-caption d-none d-md-block">
         <h5>Second slide label</h5>
         <p>Some representative placeholder content for the second slide.</p>
       </div>
     </div>
     <div className="carousel-item">
-      <img src="/img/screenTv.jpg" className="d-block w-100" alt="..."></img>
+      <img src="/img/room-1.jpg" className="d-block w-100" alt="..."></img>
       <div className="carousel-caption d-none d-md-block">
         <h5>Third slide label</h5>
         <p>Some representative placeholder content for the third slide.</p>
@@ -48,32 +48,7 @@ const Slider = () => {
   </button>
 </div>
 
-<div id="carouselExampleIndicators" className="carousel slide">
-  <div className="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img src="..." className="d-block w-100" alt="..."></img>
-    </div>
-    <div className="carousel-item">
-      <img src="..." className="d-block w-100" alt="..."></img>
-    </div>
-    <div className="carousel-item">
-      <img src="..." className="d-block w-100" alt="..."></img>
-    </div>
-  </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
-</div>
+{/* <!-- Hero Section End --> */}
 
 
 </>
