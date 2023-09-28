dd5c632f32bf01e19a7264d30e0bfd6f090e5e37
Gitari65 <agitari65@gmail.com>
2023-09-28 11:51:50 +0300

    🏘rooms  page

---
+++ vite-project/src/components/Rooms.tsx
@@ -0,0 +1,95 @@
+import React from 'react'
+
+const Rooms = () => {
+  return (
+    <div>
+         {
+            /**<!-- our_room --> */
+         } 
+      <div  className="our_room">
+         <div className="container">
+            <div className="row">
+               <div className="col-md-12">
+                  <div className="titlepage">
+                     <h2>Our Rooms</h2>
+                     <p>Lorem Ipsum available, welcome to our rooms</p>
+                  </div>
+               </div>
+            </div>
+            <div className="row">
+               <div className="col-md-4 col-sm-6">
+                  <div id="serv_hover"  className="room">
+                     <div className="room_img">
+                        <figure><img src="/img/room/rooms-1.jpg" alt="#"/></figure>
+                     </div>
+                     <div className="bed_room">
+                        <h3>Bed Room</h3>
+                        <p>If you are going to use a passage of Lorem Ipsum, you need to be sure there </p>
+                     </div>
+                  </div>
+               </div>
+               <div className="col-md-4 col-sm-6">
+                  <div id="serv_hover"  className="room">
+                     <div className="room_img">
+                        <figure><img src="/img/room/rooms-2.jpg" alt="#"/></figure>
+                     </div>
+                     <div className="bed_room">
+                        <h3>Bed Room</h3>
+                        <p>If you are going to use a passage of Lorem Ipsum, you need to be sure there </p>
+                     </div>
+                  </div>
+               </div>
+               <div className="col-md-4 col-sm-6">
+                  <div id="serv_hover"  className="room">
+                     <div className="room_img">
+                        <figure><img src="/img/room/rooms-3.jpg" alt="#"/></figure>
+                     </div>
+                     <div className="bed_room">
+                        <h3>Bed Room</h3>
+                        <p>If you are going to use a passage of Lorem Ipsum, you need to be sure there </p>
+                     </div>
+                  </div>
+               </div>
+               <div className="col-md-4 col-sm-6">
+                  <div id="serv_hover"  className="room">
+                     <div className="room_img">
+                        <figure><img src="/img/room/rooms-4.jpg" alt="#"/></figure>
+                     </div>
+                     <div className="bed_room">
+                        <h3>Bed Room</h3>
+                        <p>If you are going to use a passage of Lorem Ipsum, you need to be sure there </p>
+                     </div>
+                  </div>
+               </div>
+               <div className="col-md-4 col-sm-6">
+                  <div id="serv_hover"  className="room">
+                     <div className="room_img">
+                        <figure><img src="/img/room/rooms-5.jpg" alt="#"/></figure>
+                     </div>
+                     <div className="bed_room">
+                        <h3>Bed Room</h3>
+                        <p>If you are going to use a passage of Lorem Ipsum, you need to be sure there </p>
+                     </div>
+                  </div>
+               </div>
+               <div className="col-md-4 col-sm-6">
+                  <div id="serv_hover"  className="room">
+                     <div className="room_img">
+                        <figure><img src="/img/room/rooms-6.jpg" alt="#"/></figure>
+                     </div>
+                     <div className="bed_room">
+                        <h3>Bed Room</h3>
+                        <p>If you are going to use a passage of Lorem Ipsum, you need to be sure there </p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      {/**  <!-- end our_room --> */}
+    
+    </div>
+  )
+}
+
+export default Rooms
